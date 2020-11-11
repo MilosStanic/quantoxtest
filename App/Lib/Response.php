006d365ba144ead2582e74f4f1220e08aa3352e7
Miloš Stanić <milos.stanic@gmail.com>
2020-11-11 13:46:08 +0100

    final commit

--- App/Lib/Response.php
+++ App/Lib/Response.php
@@ -25,33 +25,13 @@ class Response
     public function toXML($data = [])
     {
         http_response_code($this->status);
-        header('Content-Type: application/xml');
-        /* $xml = new SimpleXMLElement('<root/>');
-        $result = $this->generateXML($xml, $data);
-        print_r( $xml->asXML()); */
-        // print_r($data);
-        return $this->arrayToXML($data, new SimpleXMLElement('<root/>'), 'child');
+        header('Content-Type: text/xml');        
+        return $this->simple2xml($data);
     }   
 
-    /**
-* Converts an array to XML
-*
-* @param array $array
-* @param SimpleXMLElement $xml
-* @param string $child_name
-*
-* @return SimpleXMLElement $xml
-*/
-public function arrayToXML($array, SimpleXMLElement $xml, $child_name)
-{
-    foreach ($array as $k => $v) {
-        if(is_array($v)) {
-            (is_int($k)) ? $this->arrayToXML($v, $xml->addChild($child_name), $v) : $this->arrayToXML($v, $xml->addChild(strtolower($k)), $child_name);
-        } else {
-            (is_int($k)) ? $xml->addChild($child_name, $v) : $xml->addChild(strtolower($k), $v);
-        }
+    private function simple2xml ($data){
+        $xml = new SimpleXMLElement('<root />');
+        array_walk_recursive($data, array ($xml,'addChild'));
+        return $xml->asXML();
     }
-
-    return $xml->asXML();
-}
 }
